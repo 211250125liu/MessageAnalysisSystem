159a1bec0bc95a4013dd5d778d2145a871570420
211250125liu <211250125@smail.nju.edu.cn>
2024-12-12 20:53:34 +0800

    start

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import Aside from './views/menu/index.vue'
+import Header from './views/header/index.vue'
+</script>
+
+
+<template>
+    <el-container style="width: 100vw;height: 100vh">
+        <el-aside style="width: fit-content" >
+            <Aside/>
+        </el-aside>
+        <el-container>
+            <el-header>
+                <Header/>
+            </el-header>
+            <el-main style="background-color: #535bf2">Main</el-main>
+        </el-container>
+    </el-container>
+</template>
+
+
+<style scoped>
+
+</style>
