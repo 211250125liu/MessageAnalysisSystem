efa5f212251ed885e67b03aaa9a90fe58554c7e2
211250125liu <211250125@smail.nju.edu.cn>
2025-02-14 17:18:19 +0800

    vercel尝试

--- vite.config.ts
+++ vite.config.ts
@@ -21,7 +21,7 @@ export default defineConfig({
     proxy: {
       '/api': {
         target: 'http://172.29.7.168:65348', //本地的地址
-        changeOrigin: true, // 是否改变请求源地址
+        //changeOrigin: true, // 是否改变请求源地址
         rewrite: (path) => path.replace(/^\/api/, '') // 将 /api 替换为空字符串
       }
     }
